fc90108aef24561a0a901f4521e0625563fa02de
Daniil <Git@daniil.best>
2021-06-17 16:32:49 +0300

    add string tasks

---
+++ src/main/java/ru/danka/exceptions/checker/StringChecker.java
@@ -0,0 +1,21 @@
+package ru.danka.exceptions.checker;
+
+public class StringChecker {
+    private int maxSize;
+
+    public StringChecker(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public void isNotNull(String value) throws StringCheckerException {
+
+    }
+
+    public void isNotEmpty(String value) throws StringCheckerException {
+
+    }
+
+    public void isNormalSize(String value) throws StringCheckerException {
+
+    }
+}
---
+++ src/main/java/ru/danka/exceptions/checker/StringIsEmptyException.java
@@ -0,0 +1,4 @@
+package ru.danka.exceptions.checker;
+
+public class StringIsEmptyException {
+}
